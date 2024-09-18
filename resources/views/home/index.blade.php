6d5ca0ac74a281761d42da7e6a27c6adc22bbfb5
Alfon Rifwomer Ritonga <31720644+alfonritonga@users.noreply.github.com>
2024-09-19 01:41:21 +0700

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -457,8 +457,6 @@
     <script src="{{ asset('front/js/plugins/swiper-bundle.min.js') }}"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="{{ asset('template/js/custom.min.js') }}"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- Template  JS -->
     <script src="{{ asset('front/js/main.js?v=1.0') }}"></script>
     <!-- Google tag (gtag.js) -->
